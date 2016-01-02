74148be03dd18d0ea3b1fe97bab504fe6a211a1a
Robert Beckebans <robert.beckebans@gmail.com>
2016-01-02 23:59:44 +0100

    Added bilateral blur to SSAO

--- neo/renderer/Image.h
+++ neo/renderer/Image.h
@@ -380,6 +380,7 @@ public:
 	idImage*			smaaEdgesImage;
 	idImage*			smaaBlendImage;
 	idImage*			currentNormalsImage;			// cheap G-Buffer replacement, holds normals and surface roughness
+	idImage*			currentAOImage;					// contains AO and bilateral filtering keys
 	// RB end
 	idImage* 			scratchImage;
 	idImage* 			scratchImage2;
