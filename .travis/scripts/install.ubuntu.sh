00fe10b8def3ebac6a928bdb27bc7ba9c58faacf
kordex <kordex@gmail.com>
2016-07-14 13:31:43 +0300

    update to trusty for cmake cegui 2.8.12 when clang++

--- .travis/scripts/install.ubuntu.sh
+++ .travis/scripts/install.ubuntu.sh
@@ -4,7 +4,7 @@
 
 if [ "$CXX" = "g++" ]; then sudo sed -i 's/precise/trusty/g' /etc/apt/sources.list; fi
 if [ "$CXX" = "mingw" ]; then sudo sed -i 's/precise/trusty/g' /etc/apt/sources.list; fi
-if [ "$CXX" = "clang" ]; then sudo sed -i 's/precise/trusty/g' /etc/apt/sources.list; fi
+if [ "$CXX" = "clang++" ]; then sudo sed -i 's/precise/trusty/g' /etc/apt/sources.list; fi
 
 sudo apt-get update -qq
 
@@ -12,5 +12,5 @@ sudo apt-get install libsdl1.2-dev libopenal-dev cmake
 
 if [ "$CXX" = "g++" ]; then sudo apt-get install -qqy gcc g++; fi
 if [ "$CXX" = "mingw" ]; then sudo apt-get install -qqy g++-mingw-w64-x86-64; fi
-if [ "$CXX" = "clang" ]; then sudo apt-get install -qqy clang; fi
+if [ "$CXX" = "clang++" ]; then sudo apt-get install -qqy clang; fi
 
